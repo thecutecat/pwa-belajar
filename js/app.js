87001b86086b6cbac0832a91f400551617fa3036
thecutecat <ahmad_pujianto@yahoo.com>
2019-01-22 13:17:09 +0700

    small change

--- js/app.js
+++ js/app.js
@@ -3,5 +3,5 @@ console.log('app.js');
 fetch('https://jsonplaceholder.typicode.com/users')
 .then(response => response.json())
 .then(data => {
-    console.log(data);
+   // console.log(data);
 });
